ed703458246f57cd4769148d5e609a8e6c598bd6
fwilbor <franklin.cfg@gmail.com>
2019-06-20 21:26:53 -0400

    function for 9 digit random number

--- game.js
+++ game.js
@@ -4,3 +4,10 @@ $("button").on("click", function (min, max) {
     randomize = document.getElementById("goal").innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomize
 });
+
+
+function getRandom(length) {
+
+    return Math.floor(Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1));
+
+}
