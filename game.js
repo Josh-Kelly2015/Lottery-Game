925ee883fcc4deaf707a5a9a430ae628d0713bdd
hpraveen <hariharanster@gmail.com>
2019-06-20 21:34:49 -0400

    added 9 digit number generator

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
-$("button").on("click", function (min, max) {
+/* $("button").on("click", function (min, max) {
     min = Math.ceil(0);
     max = Math.floor(9);
     randomize = document.getElementById("goal").innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomize
+}); */
+$(document).ready(function() {
+$("#random-button").on("click", function() {
+
+var lnumber = "";
+
+for (var i = 0; i < 9; i++) {
+  var randomnumber = Math.floor(Math.random() * 9) + 1; 
+  lnumber = randomnumber + lnumber;
+  console.log (lnumber);
+}
+
+});
 });
